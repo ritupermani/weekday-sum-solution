39e01f3193521512d05d4bc1a193df90733798fb
Ritu Permani <156543610+ritupermani@users.noreply.github.com>
2025-12-18 17:57:49 +0530

    Create solution.test.js

---
+++ solution.test.js
@@ -0,0 +1,44 @@
+const solution = require('./solution');
+
+test('calculates correct weekday sums', () => {
+  const input = {
+    '2020-01-01': 4,
+    '2020-01-02': 4,
+    '2020-01-03': 6,
+    '2020-01-04': 8,
+    '2020-01-05': 2,
+    '2020-01-06': -6,
+    '2020-01-07': 2,
+    '2020-01-08': -2
+  };
+
+  expect(solution(input)).toEqual({
+    Mon: -6,
+    Tue: 2,
+    Wed: 2,
+    Thu: 4,
+    Fri: 6,
+    Sat: 8,
+    Sun: 2
+  });
+});
+
+test('fills missing days using mean of neighbors', () => {
+  const input = {
+    '2020-01-01': 6,
+    '2020-01-04': 12,
+    '2020-01-05': 14,
+    '2020-01-06': 2,
+    '2020-01-07': 4
+  };
+
+  expect(solution(input)).toEqual({
+    Mon: 2,
+    Tue: 4,
+    Wed: 6,
+    Thu: 8,
+    Fri: 10,
+    Sat: 12,
+    Sun: 14
+  });
+});
